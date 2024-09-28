d4e128284e2d4c48ebc221ed3796eb7289046458
George Galanakis <ggalan87@gmail.com>
2024-09-28 17:42:43 +0300

    remote url

--- ardour_control.py
+++ ardour_control.py
@@ -1,8 +1,5 @@
 import argparse
-import random
 import time
-import numpy as np
-import json
 import requests
 import subprocess
 
@@ -22,22 +19,25 @@ def kill(pid):
         pass
 
 
-def switch_faders(fader_on_id, fader_off_id):
+def switch_faders(ardour_server, ardour_port, fader_on_id, fader_off_id):
     def send(fader, db):
-        subprocess.run(["oscsend", "localhost", "3819", "/strip/gain", "if", str(fader), str(db)])
+        subprocess.run(["oscsend", f'{ardour_server}', f'{ardour_port}', "/strip/gain", "if", str(fader), str(db)])
 
-    #
+    # Precomputed using numpy's geometric progression:
     inf_db = -193
-    min_db = -50
-    regular_db = 0
-    offset = inf_db
-    increasing_dbs = np.geomspace(min_db + offset, regular_db + offset, num=15, dtype=np.int32) - offset
+    # min_db = -50
+    # regular_db = 0
+    # offset = inf_db
+    # increasing_dbs = list(np.geomspace(min_db + offset, regular_db + offset, num=15, dtype=np.int32) - offset)
 
-    for i, d in zip(increasing_dbs, np.flip(increasing_dbs)):
+    increasing_dbs = [-50, -46, -42, -38, -34, -30, -27, -23, -20, -16, -13, -9, -6, -3, 0]
+
+    for i, d in zip(increasing_dbs, increasing_dbs[::-1]):
         send(fader_on_id, i)
         send(fader_off_id, d)
         time.sleep(0.1)
     send(fader_off_id, inf_db)
+
 
 def str2bool(v):
     if isinstance(v, bool):
@@ -53,19 +53,9 @@ def str2bool(v):
 
 def main(args):
     if args.reset:
-        switch_faders(fader_on_id=args.autopilot_fader_id, fader_off_id=args.vlc_fader_id)
+        switch_faders(ardour_server=args.ardour_ip, ardour_port=args.ardour_port,
+                      fader_on_id=args.autopilot_fader_id, fader_off_id=args.vlc_fader_id)
         print('Just switched to autopilot')
-        return
-
-    r = requests.get(args.local_icecast + "/status-json.xsl")
-    data = r.json()
-    try:
-        if isinstance(data, list):
-            data = data[0]
-        listen_url = data['icestats']['source']['listenurl']
-        #print(listen_url)
-    except KeyError:
-        print('Stream not available.')
         return
 
     # Check existing instance
@@ -75,15 +65,23 @@ def main(args):
         if len(pids) > 0:
             print('Remote was already up')
         else:
-            cmd = f'nohup cvlc -q {listen_url} > /dev/null > /dev/null 2>&1 &'
+            r = requests.get(args.stream_url, stream=True)
+            if not r.ok:
+                print('Stream not available.')
+                return
+
+            cmd = f'nohup cvlc -q {args.stream_url} > /dev/null > /dev/null 2>&1 &'
             subprocess.Popen(['/bin/bash', '-c', cmd])
-            switch_faders(fader_on_id=args.vlc_fader_id, fader_off_id=args.autopilot_fader_id)
+
+            switch_faders(ardour_server=args.ardour_ip, ardour_port=args.ardour_port,
+                          fader_on_id=args.vlc_fader_id, fader_off_id=args.autopilot_fader_id)
             print('Remote started.')
     else:
         if len(pids) > 0:
             for pid in pids:
                 kill(pid)
-            switch_faders(fader_on_id=args.autopilot_fader_id, fader_off_id=args.vlc_fader_id)
+            switch_faders(ardour_server=args.ardour_ip, ardour_port=args.ardour_port,
+                          fader_on_id=args.autopilot_fader_id, fader_off_id=args.vlc_fader_id)
             print('Remote stopped, scheduler is back.')
         else:
             print('Remote was already off.')
@@ -95,13 +93,15 @@ if __name__ == "__main__":
                         help="The ip of the OSC server")
     parser.add_argument("--ardour-port", type=int, default=3819,
                         help="The port the OSC server is listening on")
-    parser.add_argument("--local-icecast", type=str, default="http://rastapank.radio.uoc.gr:8000")
-    parser.add_argument("--remote", type=str2bool, default=True,
-                        help="Switch on or off")
     parser.add_argument("--autopilot-fader-id", type=int, default=6,
                         help="The ardour id of the autopilot fader")
     parser.add_argument("--vlc-fader-id", type=int, default=3,
                         help="The ardour id of the vlc fader")
+
+    parser.add_argument("--stream-url", type=str, default="http://rastapank.radio.uoc.gr:8000/live")
+
+    parser.add_argument("--remote", type=str2bool, default=True,
+                        help="Switch on or off")
     parser.add_argument("--reset", action='store_true',
                         help="The ardour id of the vlc fader")
 
